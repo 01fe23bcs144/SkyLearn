d4b603bdfc893671ba8872de369632c9752f169e
Bhagyashree Yadagiri <168655062+01fe23bcs144@users.noreply.github.com>
2025-11-13 20:57:45 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -270,3 +270,7 @@ SEMESTER_CHOICES = (
     (SECOND, _("Second")),
     (THIRD, _("Third")),
 )
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
